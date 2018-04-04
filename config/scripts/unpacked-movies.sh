79e7d69a8e9faba2e55c239cecb31fb07db5f30c
Paaff <pafischer1992@gmail.com>
2018-04-04 20:52:54 +0200

    Refactored unpacked-movies script to use find command. Works like a charm now

--- config/scripts/unpacked-movies.sh
+++ config/scripts/unpacked-movies.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 # Argument provided by user. Expected to be a directory path.
 if [[ -z $1 ]] ; then
@@ -6,27 +6,27 @@ if [[ -z $1 ]] ; then
 	exit 1
 fi
 
-PATH="$1"
-
-# Function to check whether the or not the directory has a mkv file in it.
-hasMKV() {
-    for f in $1/*; do
-        case ${f: -4} in
-            ".mkv" | ".avi" | ".iso" | ".ISO")
-                return 1 ;;
-            *)
-        esac
+no_ext() {
+    for f  in "$1"/*; do
+        if [ -d "$f" ]; then
+            # Just a subdir, no need to check here.
+            continue
+        fi
+            EXT="${f##*.}"
+            case $EXT in
+                "mkv" | "avi" | "iso" | "ISO")
+                    # Known video extensions found.
+                    return 1;;
+                *)
+            esac
     done
-    
-    # No .mkv files found, return the path so we know which dir it is.
-    echo $1
+    # No video extensions found, return true.
+    return 0
 }
 
 # Go into each folder in the directory given.
-for dir in $1/*; do 
-    if [ -d "$dir" ]; then
-        hasMKV $dir;
+find $1 -maxdepth 1 -type d ! -path $1 | while read dir; do
+    if no_ext "$dir"; then
+        echo "$dir"
     fi
 done
-
-
